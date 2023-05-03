c616050c4a7e055be9bda3126c5571afa57e3d73
LKTOKB <theodiamanttt@gmail.com>
2023-05-03 11:54:06 +0300

    Offer model create, price and optionnal message, connect by id to each user and product

---
+++ server/models/Offer.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+
+const offerSchema = new Schema(
+    {
+        price: {
+          type: Number,
+          required: [true, "Price is required."],
+        },
+
+      message: { type: String },
+
+      product: [{type: Schema.Types.ObjectId, ref: "Product"}],
+      user: [{type: Schema.Types.ObjectId, ref: "User"}],
+    },
+    {
+      // this second object adds extra properties: `createdAt` and `updatedAt`
+      timestamps: true,
+    }
+  );
+  
+  const Offer = model("Offer", offerSchema);
+  
+  module.exports = Offer;
+  
