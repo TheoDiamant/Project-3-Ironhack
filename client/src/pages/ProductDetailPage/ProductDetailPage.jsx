6bfa27093c7fca75be53dcb78b6663e363e0ba91
brea-practice <ihk.gitcomm@gmail.com>
2023-05-07 18:35:25 +0200

    ProductDetailsPage redesign - WIP

--- client/src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ client/src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,14 +1,16 @@
+import "./ProductDetailPage.css"
+
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+
+
 import Offer from "../../components/Offer/Offer";
 import Review from "../../components/Review/Review";
-import ProductCard from "../../components/ProductCard/ProductCard"
 import YouMightAlsoLike from "../../components/YouMightAlsoLike/YouMightAlsoLike"
 
+import axios from "axios";
 
 const API_URL = "http://localhost:5005";     
-
 
 function ProductDetailPage() {
 
@@ -22,6 +24,13 @@ function ProductDetailPage() {
 
 
     const { productId } = useParams(); 
+
+    useEffect(() => {
+        getProduct();
+        getReview();
+        getOffer();
+        getUser();
+    }, []);
 
     const getProduct = () => {
         axios.get(`${API_URL}/api/products/${productId}`)
@@ -61,166 +70,76 @@ function ProductDetailPage() {
         })
       }
 
-    useEffect(() => {
-        getProduct();
-        getReview();
-        getOffer();
-        getUser();
-    }, []);
-
     return (
-        
-        <div>
+        <div className="productDetailsBodyDiv">
+            <div className="productDetailsDiv">
 
-        <main className="main-offer">
-
-        <div className="wrapper offer-page">
-            <div className="carousel">
-
-            {/* {product === null ? <p>Loading images</p> : <img src={product.img}/> } */}
-
-            {product === null ? <p> Loading Images </p> : product.img.map(image => {
-                return (
-                    <img src={image}/>
-                )
-       
-            })}
-        
-            </div>
-          
-
-
-            <div className="offer-page-details">
-
-            {product === null ? <p>Loading price</p> : <p className="offer-price">{product.price}€</p> }
-
-            <hr className="hr-design"></hr>
-
-    
-            {product === null ? <p>Loading title</p> :<p className="text-title">{product.title}</p>  }
-
-
-            <div class="bloc-1">
-                <div>
-                    <p className="description-details">
-                        <span>Brand :</span>
-                        <span><strong>Zara</strong></span>
-                    </p>
-                    <p className="description-details">
-                        <span>Size :</span>
-                        <span><strong>L</strong></span>
-                    </p>
-                    <p className="description-details">
-                        <span>State :</span>
-                        <span><strong>New</strong></span>
-                    </p>
-                    <p className="description-details">
-                        <span>Color :</span>
-                        <span><strong>Blue</strong></span>
-                    </p>
-                    <p className="description-details">
-                        <span>Country :</span>
-                        <span><strong>France</strong></span>
-                    </p>
+                <div className="carousel">
+                    {product === null ? <p> Loading Images </p> : product.img.map(image => {
+                        return (
+                            <img src={image} alt=""/>
+                        )
+                    })}
                 </div>
-          </div>
-                    <div className="bloc-2">
-
+                
+                <div className="offer-page-details">
+                    {product === null ? <p>Loading price</p> : <p className="offer-price">{product.price}€</p> }
                     <hr className="hr-design"></hr>
-                    
-            <h4>Product details :</h4>
-            {product === null ? <p>Loading description</p> :<p className="description-general">Quarto et exilium pater non non locum est cruribus pater exilium per cum quarto in nulla lapide et exilium pater actitata in et nulla cum nomine multiplices Antiochia ut ut nulla et locum pervenissent ambo in quaedam cum pater quaedam et igitur clades lapide scilicet in Antiochia vicensimo clades lapide.</p>  }
-                    
-                      
-              <hr className="hr-design"></hr>
+                    {product === null ? <p>Loading title</p> :<p className="text-title">{product.title}</p>  }
+                    <div class="bloc-1">
                         <div>
-                        {/* put here the original picture of the user */}
-                            <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>
-
-                    <div className="name-review">  
-                     {user === null ? <p>Loading name</p> : <h1 className="username-details-page">{user.name}</h1>}
-                    <p> Reviews : 
-                        {user && user.review ? (user.review.length === 0 ? <p> No reviews yet</p> : user.review.length) : ( <p> Loading Reviews..</p>)} ⭐️
-                        </p>                     
-                    </div>
-
+                            <p className="description-details">
+                                <span>Brand :</span>
+                                <span><strong>Zara</strong></span>
+                            </p>
+                            <p className="description-details">
+                                <span>Size :</span>
+                                <span><strong>L</strong></span>
+                            </p>
+                            <p className="description-details">
+                                <span>State :</span>
+                                <span><strong>New</strong></span>
+                            </p>
+                            <p className="description-details">
+                                <span>Color :</span>
+                                <span><strong>Blue</strong></span>
+                            </p>
+                            <p className="description-details">
+                                <span>Country :</span>
+                                <span><strong>France</strong></span>
+                            </p>
                         </div>
                     </div>
+                    <div className="bloc-2">
+                        <hr className="hr-design"></hr>
+                        <h4>Product details :</h4>
+                        {product === null ? <p>Loading description</p> :<p className="description-general">Quarto et exilium pater non non locum est cruribus pater exilium per cum quarto in nulla lapide et exilium pater actitata in et nulla cum nomine multiplices Antiochia ut ut nulla et locum pervenissent ambo in quaedam cum pater quaedam et igitur clades lapide scilicet in Antiochia vicensimo clades lapide.</p>}  
+                        <hr className="hr-design"></hr>
 
-                   
-
-                <div className="bloc-1">
-                    <div>
-                        <p>Country : France</p>
-                        <p>Last seen: 45 min ago..</p>
+                        <div>
+                            {/* put here the original picture of the user */}
+                            <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg" alt=""/>
+                            <div className="name-review">  
+                                {user === null ? <p>Loading name</p> : <h1 className="username-details-page">{user.name}</h1>}
+                                <p> Reviews : {user && user.review ? (user.review.length === 0 ? <p> No reviews yet</p> : user.review.length) : ( <p> Loading Reviews..</p>)} ⭐️</p>                     
+                            </div>
+                        </div>
                     </div>
+                    <div className="bloc-1">
+                        <div>
+                            <p>Country : France</p>
+                            <p>Last seen: 45 min ago..</p>
+                        </div>
+                    </div>
+                    <button className="btn-green">Message</button>
+                    <button className="btn-border-green">Make an Offer</button>
+                    <button className="btn-border-green">Payment</button>
+                    <button className="btn-border-green">Add to WishList ❤️ </button>
                 </div>
-
-                    <button class="btn-green">Message</button>
-                    <button class="btn-border-green">Make an Offer</button>
-                    <button class="btn-border-green">Payment</button>
-                    <button class="btn-border-green">Add to WishList ❤️ </button>
-
-
+                {/* <div>
+                    <YouMightAlsoLike /> 
+                </div> */}
             </div>
-        </div>
-        <div>
-
-            <YouMightAlsoLike />
-            
-        </div>
-
-        </main>
-
-            {/* {product &&  (
-                <>
-                {product.img.map(image => {
-                    return(
-                        <img src={image} alt="" />
-                    )
-                })}
-                    <h1>{product.title}</h1>
-                    <p>{product.description}</p>
-                    <p>{product.price}</p>
-                </>
-            )}
-
-
-            <Link to="/products">
-                <button>Back to products</button>
-            </Link>
-
-            <Link to={`/products/${productId}/edit`}>
-                <button>Edit Product</button>
-            </Link>  
-
-            <Offer />
-
-            <Review />
-
-            {review === null ? <p>Loading reviews...</p> : review.map(reviews => {
-                return (
-                    <div key={reviews._id}>
-                    <img src={reviews.img}/>
-                        <h1>{reviews.title}</h1>
-                        <p>{reviews.message}</p>
-                    </div>
-                )
-            })}
-            {offer === null ? <p>Loading offers...</p> : offer.map(offers => {
-                return (
-                    <div key={offers._id}>
-                        <h1>{offers.price}</h1>
-                        <p>{offers.message}</p>
-                    </div>
-                )
-            })} */}
-
-
-
-
-
-
         </div>
     )
 }
