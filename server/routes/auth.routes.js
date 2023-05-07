22f2f10000a8af5576bb398c59cba0073e409e21
brea-practice <ihk.gitcomm@gmail.com>
2023-05-07 12:06:06 +0200

    Super minor changes to html tag attributes to make them React compliant

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -33,88 +33,76 @@ router.post("/signup", (req, res, next) => {
     return;
   }
 
-  // Check the users collection if a user with the same email already exists
   User.findOne({ email })
     .then((foundUser) => {
-      // If the user with the same email already exists, send an error response
       if (foundUser) {
         res.status(400).json({ message: "User already exists." });
         return;
       }
 
-      // If email is unique, proceed to hash the password
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
 
-      // Create the new user in the database
-      // We return a pending promise, which allows us to chain another `then`
       return User.create({ email, password: hashedPassword, name });
     })
     .then((createdUser) => {
-      // Deconstruct the newly created user object to omit the password
-      // We should never expose passwords publicly
+      
       const { email, name, _id } = createdUser;
 
-      // Create a new object that doesn't expose the password
       const user = { email, name, _id };
 
-      // Send a json response containing the user object
       res.status(201).json({ user: user });
     })
-    .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+    .catch((err) => next(err));
 });
 
-// POST  /auth/login - Verifies email and password and returns a JWT
+
 router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
 
-  // Check if email or password are provided as empty string
+
   if (email === "" || password === "") {
     res.status(400).json({ message: "Provide email and password." });
     return;
   }
 
-  // Check the users collection if a user with the same email exists
+
   User.findOne({ email })
     .then((foundUser) => {
       if (!foundUser) {
-        // If the user is not found, send an error response
+        
         res.status(401).json({ message: "User not found." });
         return;
       }
 
-      // Compare the provided password with the one saved in the database
+      
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
       if (passwordCorrect) {
-        // Deconstruct the user object to omit the password
+        
         const { _id, email, name } = foundUser;
 
-        // Create an object that will be set as the token payload
+        
         const payload = { _id, email, name };
 
-        // Create a JSON Web Token and sign it
+        
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
           algorithm: "HS256",
           expiresIn: "6h",
         });
 
-        // Send the token as the response
+        
         res.status(200).json({ authToken: authToken });
       } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
     })
-    .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+    .catch((err) => next(err)); 
 });
 
-// GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get("/verify", isAuthenticated, (req, res, next) => {
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and is made available on `req.payload`
-  // console.log(`req.payload`, req.payload);
 
-  // Send back the token payload object containing the user data
+router.get("/verify", isAuthenticated, (req, res, next) => {
+ 
   res.status(200).json(req.payload);
 });
 
