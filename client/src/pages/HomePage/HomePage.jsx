92dd360589a1003944907512b871c8eee649bbdc
brea-practice <ihk.gitcomm@gmail.com>
2023-05-07 17:16:12 +0200

    Merging

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
   return (
     <div>
       <div className="firstSection"> 
-        <img class="banner" src="https://myvinted-lereacteur-2021.netlify.app/static/media/hero-banner.bebb87dd.jpg"/>
+        <img class="banner" src="https://myvinted-lereacteur-2021.netlify.app/static/media/hero-banner.bebb87dd.jpg" alt=""/>
       </div>
       <div className="secondSection">
         <h1 className="latestProductsText">Latest Products</h1>
