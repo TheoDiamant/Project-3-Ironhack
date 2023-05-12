15337d30f4c80289dbe76d3a2b9b9e2f84ed519c
brea-practice <ihk.gitcomm@gmail.com>
2023-05-12 20:59:22 +0200

    Cart total price implemented

--- client/src/components/CartSideBar/CartSideBar.jsx
+++ client/src/components/CartSideBar/CartSideBar.jsx
@@ -7,23 +7,27 @@ function CartSideBar() {
     
     const { cart, removeFromCart } = useContext(CartContext)
 
-    
+    let cartTotal = 0
+    cart.forEach(item => {
+        cartTotal += item.price
+    });
 
     return(
         <div className="cartMainDiv">
-            <h5 className="cartName">Your Shopping Cart</h5>
+            <div className="cartTop">
+                <h5 className="cartName">Your Shopping Cart</h5>
+                <h5>Your total is: {cartTotal}€</h5>
+            </div>
             {cart.map(item => {
                 return(
                     <div key={item._id} className="cartItemDiv">
                         <img className="cartImage" src={item.img[0]} alt="" />
                         <div className="cartInfo">
                             <div className="cartTitleAndPrice">
+                                <div className="hackyDivider"></div>
                                 <h6>{item.title}</h6>
                                 <hr />
-                                <p>{item.price}€</p>
-                            </div>
-                            <div className="cartQuantity">
-                                <span>x 1</span>
+                                <h6>{item.price}€</h6>
                             </div>
                             <div className="cartDelete">
                                 <button className="cartDeleteButton" onClick={() => removeFromCart(item._id)}></button> 
