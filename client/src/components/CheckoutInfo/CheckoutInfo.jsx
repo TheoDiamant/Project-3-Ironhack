7e922e0abf14bc15ed39ff746ac74f0e74476f20
brea-practice <ihk.gitcomm@gmail.com>
2023-05-13 14:44:33 +0200

    Checkout info controlled, button to clone

--- client/src/components/CheckoutInfo/CheckoutInfo.jsx
+++ client/src/components/CheckoutInfo/CheckoutInfo.jsx
@@ -1,6 +1,11 @@
 import "./CheckoutInfo.css"
 
-function CheckoutInfo() {
+function CheckoutInfo({ info, setInfo }) {
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setInfo(prevState => ({...prevState, [name]: value}))
+    }
 
     return(
         <div className="checkoutInfoWrapper">
@@ -8,48 +13,48 @@ function CheckoutInfo() {
             <div className="checkoutGrouper">
                     <div className="checkoutInfoDiv">
                         <label className="checkoutLabel">First Name</label>
-                        <input className="checkoutInput" type="text" name="firstName" placeholder="John"/>
+                        <input value={info.firstName} onChange={handleChange} className="checkoutInput" type="text" name="firstName" placeholder="John"/>
                     </div>
 
                     <div className="checkoutInfoDiv">
                         <label className="checkoutLabel">Last Name</label>
-                        <input className="checkoutInput" type="text" name="lastName" placeholder="Doe"/>
+                        <input value={info.lastName} onChange={handleChange} className="checkoutInput" type="text" name="lastName" placeholder="Doe"/>
                     </div>
             </div>
 
             <div className="checkoutGrouper">
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">Address 1</label>
-                    <input className="checkoutInput" type="text" name="address1" placeholder="Street"/>
+                    <input value={info.address1} onChange={handleChange} className="checkoutInput" type="text" name="address1" placeholder="Street"/>
                 </div>
             </div>
             <div className="checkoutGrouper">
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">Address 2</label>
-                    <input className="checkoutInput" type="text" name="address2" placeholder="Number, Apartment, Stairs"/>
+                    <input value={info.address2} onChange={handleChange} className="checkoutInput" type="text" name="address2" placeholder="Number, Apartment, Stairs"/>
                 </div>
             </div>
             <div className="checkoutGrouper">
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">Country</label>
-                    <input className="checkoutInput" type="text" name="country" placeholder="Worldwide"/>
+                    <input value={info.country} onChange={handleChange} className="checkoutInput" type="text" name="country" placeholder="Worldwide"/>
                 </div>
             </div>
 
             <div className="checkoutGrouper">
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">City</label>
-                    <input className="checkoutInput" type="text" name="city" placeholder="Athens"/>
+                    <input value={info.city} onChange={handleChange} className="checkoutInput" type="text" name="city" placeholder="Athens"/>
                 </div>
 
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">State/Province</label>
-                    <input className="checkoutInput" type="text" name="state" placeholder="Arkansas"/>
+                    <input value={info.state} onChange={handleChange} className="checkoutInput" type="text" name="state" placeholder="Arkansas"/>
                 </div>
 
                 <div className="checkoutInfoDiv">
                     <label className="checkoutLabel">Zip Code</label>
-                    <input className="checkoutInput" type="number" name="zipCode" placeholder="#####"/>
+                    <input value={info.zipCode} onChange={handleChange} className="checkoutInput" type="test" name="zipCode" placeholder="#####"/>
                 </div>
             </div>
 
--- client/src/pages/Checkout/Checkout.jsx
+++ client/src/pages/Checkout/Checkout.jsx
@@ -1,24 +1,53 @@
 import "./Checkout.css"
 
 import CheckoutInfo from "../../components/CheckoutInfo/CheckoutInfo"
+import { useState } from "react"
 
 function Checkout() {
+
+    const [billingInfo, setBillingInfo] = useState({
+        firstName: "",
+        lastName: "",
+        address1: "",
+        address2: "",
+        country: "",
+        city: "",
+        state: "",
+    })
+
+    const [shippingInfo, setShippingInfo] = useState({
+        firstName: "",
+        lastName: "",
+        address1: "",
+        address2: "",
+        country: "",
+        city: "",
+        state: "",
+    })
+
+    function cloneInfo() {
+        setShippingInfo(billingInfo)
+    }
 
     return(
         <div className="checkoutPageDiv">
             <div className="checkoutWrapper">
 
                 <div className="checkoutDetails">
-
-                    <CheckoutInfo />
-
-                    <div className="shippingDetails">
-
+                    <div className="checkoutDetailsWrapper">
+                        <h3>Billing information</h3>
+                        <CheckoutInfo info={billingInfo} setInfo={setBillingInfo}/>
                     </div>
-
+                    <button className="sameInfoButton">
+                        <img onClick={cloneInfo} src="https://i.imgur.com/DQBXyxE.png" alt="" />
+                    </button>
+                    <div className="checkoutDetailsWrapper">
+                        <h3>Shipping information</h3>
+                        <CheckoutInfo info={shippingInfo} setInfo={setShippingInfo}/>
+                    </div>
                 </div>
 
-                <div className="summaryAndPayment">
+                {/* <div className="summaryAndPayment">
 
                     <div className="cartSummary">
 
@@ -27,7 +56,7 @@ function Checkout() {
 
                     </div>
 
-                </div>
+                </div> */}
 
             </div>
         </div>
