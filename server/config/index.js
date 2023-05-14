74f374eb360299ae7d1785a2c56f3e4d025d809a
LKTOKB <theodiamanttt@gmail.com>
2023-05-14 20:27:20 +0300

    New depencies

--- server/config/index.js
+++ server/config/index.js
@@ -13,6 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
+
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
 // Middleware configuration
@@ -20,6 +21,8 @@ module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
+
+
 
   // controls a very specific header to pass headers from the frontend
   app.use(
@@ -30,6 +33,7 @@ module.exports = (app) => {
 
   // In development environment the app logs
   app.use(logger("dev"));
+
 
   // To have access to `body` property in the request
   app.use(express.json());
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 const User = require("../models/User.model");
+
+
 
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
