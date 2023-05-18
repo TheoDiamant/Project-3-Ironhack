60d393afb72bfe3e7c99415df8edb2c5954b18f1
brea-practice <ihk.gitcomm@gmail.com>
2023-05-18 14:43:32 +0200

    Scrolling to bottom works

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -2,19 +2,22 @@ import "./ChatBox.css"
 
 import axios from "axios"
 
-import React, { useState, useEffect, useContext } from 'react'
-import ScrollToBottom from "react-scroll-to-bottom"
+import React, { useState, useEffect, useContext, useRef } from 'react'
 import { AuthContext } from "../../context/auth.context"
 
 import { io } from "socket.io-client"
 
 const API_URL = "http://localhost:5005"
 
+const socket = io("http://localhost:5000", {
+    autoConnect: false //if not set to false, will connect to server every time component reloads
+})
+
 function ChatBox({ singleChat })  {
-
-    const { messageHistory } = singleChat
-
+    
+    const [allMessages, setAllMessages] = useState(singleChat.messageHistory)
     const [newMessage, setNewMessage] = useState("")
+    const chatBoxRef = useRef(null)
 
     const { user } = useContext(AuthContext)
 
@@ -30,28 +33,48 @@ function ChatBox({ singleChat })  {
         ownPFP = singleChat.users[1].profilePicture
     }
 
-
     //This useEffect adds an event listener for the enter key to send messages and joins a socket room whose ID is determined by the MongoDB Room document - this makes each room unique and avoids user collisions
+    
     useEffect(() => {
-        document.addEventListener("keydown", handleEnterKeypress)
-        // const socket = io("http://localhost:5000")
-        // socket.emit("join_room", singleChat._id)
+        //we connect to the client, we join a room, and we set up an event listener to receive messages
+        socket.connect()
+        socket.emit("join_room", singleChat._id)
 
-        // return () => {
-        //     socket.emit("disconnect")
-        // }
+        socket.on("receive_message", data => {
+            console.log(data)
+            const socketMessage = {
+                content: data.content,
+                sender: data.sender,
+                timestamp: data.timestamp
+            }
+            setAllMessages(prevState => [...prevState, socketMessage])
+        })
+
         return () => {
-            document.removeEventListener("keydown", handleEnterKeypress)
+            socket.disconnect()
         }
-    })
+    }, [])
+
+    //UseEffect that scrolls to the bottom when a new message loads
+    useEffect(() => {
+        scrollToBottom()
+    }, [allMessages])
 
     function handleEnterKeypress(e) {
         if(newMessage === "") {
             return
         }
-
+        
         if(e.key === "Enter") {
-            // socket.emit("send_message", newMessage)
+            const socketData = {
+                room: singleChat._id,
+                content: newMessage,
+                sender: user._id,
+                timestamp: new Date().toISOString()
+            }
+            setAllMessages(prevState => [...prevState, socketData])
+            socket.emit("send_message", socketData)
+            
             const messageToStore = {
                 content: newMessage,
                 sender: user._id,
@@ -66,78 +89,51 @@ function ChatBox({ singleChat })  {
         setNewMessage(e.target.value)
     }
 
+    function scrollToBottom() {
+        chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight
+    }
+
     return (
-
         <>
-            <div className="messagesDiv">
-                <ScrollToBottom className="scrollBottom">
-                    {messageHistory.map((message) => {
-                        message.timestamp = new Date(message.timestamp).toLocaleTimeString().substring(0, 5)
-                        return (
-                            <div className={message.sender === user._id ? "singleMessageDiv yourOwn" : "singleMessageDiv otherUser"}>
-                                
-                                {message.sender === user._id
-                                
-                                ?
+            <div className="messagesDiv" ref={chatBoxRef}>
+                {allMessages.map((message) => {
+                    const formattedTime = new Intl.DateTimeFormat(undefined, {
+                        hour: '2-digit',
+                        minute: '2-digit'
+                    }).format(new Date(message.timestamp))
 
-                                <div className="messageWrapper">
-                                    <p className="messageTime">{message.timestamp}</p>
-                                    <p className="messageText">{message.content}</p>
-                                    <img className="messagePFP" src={ownPFP} alt="" />
-                                </div>                               
+                    return (
+                        <div key={message._id} className={message.sender === user._id ? "singleMessageDiv yourOwn" : "singleMessageDiv otherUser"}>
+                            
+                            {message.sender === user._id
+                            
+                            ?
 
-                                :
+                            <div className="messageWrapper">
+                                <p className="messageTime">{formattedTime}</p>
+                                <p className="messageText">{message.content}</p>
+                                <img className="messagePFP" src={ownPFP} alt="" />
+                            </div>                               
 
-                                <div className="messageWrapper">
-                                    <img className="messagePFP" src={otherUserPFP} alt="" />
-                                    <p className="messageText">{message.content}</p>
-                                    <p className="messageTime">{message.timestamp}</p>
-                                </div>
+                            :
 
-                                }
-                               
+                            <div className="messageWrapper">
+                                <img className="messagePFP" src={otherUserPFP} alt="" />
+                                <p className="messageText">{message.content}</p>
+                                <p className="messageTime">{formattedTime}</p>
                             </div>
-                        )
-                    })}
-                </ScrollToBottom>
+
+                            }
+                            
+                        </div>
+                    )
+                })}
             </div>
 
             <div className="messageInputDiv">
-                <input className='chatInput' value={newMessage} type="text" placeholder="Send a message" onChange={storeNewMesage}/>
+                <input className='chatInput' value={newMessage} type="text" placeholder="Send a message" onChange={storeNewMesage} onKeyDown={handleEnterKeypress}/>
             </div>
         </>
-        // <div className='chat-window'>
-
-        //     <div className='chat-header'>
-        //         <p>Live Chat</p>
-        //     </div>
-        //     <div className='chat-body'>
-            // <ScrollToBottom className='message-container'>
-            //     {messageList.map((messageContent) => {
-            //         return (
-            //             <div className='message' id={username === messageContent.author ? "you" : "other"}>
-            //                 <div>
-            //                 <div className='message-content'>
-            //                     <p>{messageContent.message}</p>
-            //                 </div>
-            //                 <div className='message-meta'>
-            //                     <p id="time">{messageContent.time}</p>
-            //                     <p id="author">{messageContent.author}</p>
-            //                 </div>
-            //                 </div>
-            //             </div>
-            //         )
-            //     })}
-            //     </ScrollToBottom>
-        //     </div>
-        //     <div className='chat-footer'>
-        //         <input type='text' value={currentMessage} placeholder='Type a message' onChange={(e) => {setCurrentMessage(e.target.value)}} onKeyPress={(e) => {e.key === "Enter" && sendMessage()}}/>
-        //         <button onClick={sendMessage}>&#9658;</button>
-        //     </div>
-
-            
-        // </div>
-
     )
 
 }
