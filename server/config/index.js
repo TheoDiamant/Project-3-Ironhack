74f1dc4c5842a1b7e652e8d5f4625e52beabe9b1
brea-practice <ihk.gitcomm@gmail.com>
2023-05-25 17:27:21 +0200

    CORS testing

--- server/config/index.js
+++ server/config/index.js
@@ -27,7 +27,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: "*",
+      origin: process.env.ORIGIN,
     })
   );
 
