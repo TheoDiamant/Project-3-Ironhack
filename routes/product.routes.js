6232291994843d2a8e03c9bca7f3dadff4cf3484
brea-practice <ihk.gitcomm@gmail.com>
2023-05-26 16:36:50 +0200

    Cleaned up routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose')
 
 const Product = require("../models/Products.model")
 const User = require("../models/User.model")
-const Offer = require("../models/Offer.model")
 const Like = require("../models/Like.model")
 
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
@@ -21,36 +19,38 @@ router.post("/uploadmany", fileUploader.array("image"), (req, res, next) => {
   res.json(imagePaths)
 })
 
+
 // Route to upload a single image to the cloud - used for profile picture change
 router.post("/upload", fileUploader.single("image"), (req, res, next) => {
   res.json(req.file.path);
 });
 
+
 // Route to create a products  //////// WORK  ////////
 router.post("/products", isAuthenticated, (req, res, next) => {
 
-    Product.create({ ...req.body, user: req.payload._id  })
-    .then(response => {
+  Product.create({ ...req.body, user: req.payload._id  })
+  .then(response => {
 
-      res.json(response)
+    res.json(response)
 
-      return User.findByIdAndUpdate(req.payload._id, {
-        $push: {product: response._id}},
-        {new: true})
-    })
-    .catch(err => res.json(err))
+    return User.findByIdAndUpdate(req.payload._id, {
+      $push: {product: response._id}},
+      {new: true})
+  })
+  .catch(err => res.json(err))
 })
 
-// Route to get all the products //////// WORK  ////////
 
+// Route to get all the products //////// WORK  ////////
 router.get("/products", (req, res, next) => {
 
   Product.find()
-    .populate("user")
-    .then(allProducts => {
-      res.json(allProducts)
-    })
-    .catch(err => res.json(err))
+  .populate("user")
+  .then(allProducts => {
+    res.json(allProducts)
+  })
+  .catch(err => res.json(err))
 })
 
 
@@ -60,28 +60,27 @@ router.get("/preview", (req, res, next) => {
   const { q } = req.query
 
   Product.find({ $or: [{ title: {$regex: q, $options: "i" } }, { description: {$regex: q, $options: "i" } }] })
-    .populate("user")
-    .then(allProducts => {
-      allProducts = allProducts.slice(0, 3) 
-      res.json(allProducts)
-    })
-    .catch(err => res.json(err))
+  .populate("user")
+  .then(allProducts => {
+    allProducts = allProducts.slice(0, 3) 
+    res.json(allProducts)
+  })
+  .catch(err => res.json(err))
 })
 
 
 // Route to get a product by ID  //////// WORK  ////////
-
 router.get("/products/:productId", (req, res, next) => {
 
   const { productId } = req.params
 
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-
   Product.findById(productId)
-  .populate("user")
+  .populate({
+    path: "products",
+    populate: {
+      path: "user",
+    }
+  })
   .then(product => {
     res.status(200).json(product)
   })
@@ -89,98 +88,41 @@ router.get("/products/:productId", (req, res, next) => {
 
 })
 
-// Route to get several products by ID for the initial cart load
 
+// Route to get several products by ID for the initial cart load
 router.post("/cart", (req, res, next) => {
+
   const productIds = req.body
 
   Product.find({ _id: { $in: productIds } })
-    .then(response => res.json(response))
-    .catch(err => res.json(err))
+  .then(response => res.json(response))
+  .catch(err => res.json(err))
 })
 
 
 // Route to UPDATE/EDIT a product by ID //////// WORK  ////////
 router.post("/products/:productId/edit", (req, res, next) => {
 
-    const { productId } = req.params
+  const { productId } = req.params
 
-    if (!mongoose.Types.ObjectId.isValid(productId)) {
-        res.status(400).json({ message: 'Specified id is not valid' });
-        return;
-      }
+  
 
-    Product.findByIdAndUpdate(productId, req.body, { new: true })
-    .then((updateProduct) => res.json(updateProduct))
-    .catch(error => res.json(error))
-
+  Product.findByIdAndUpdate(productId, req.body, { new: true })
+  .then((updateProduct) => res.json(updateProduct))
+  .catch(error => res.json(error))
 })
 
 
 // Route to DELETE a product by ID //////// WORK  ////////
-
 router.delete("/products/:productId", (req, res, next) => {
 
-    const { productId } = req.params
-
-    if (!mongoose.Types.ObjectId.isValid(productId)) {
-        res.status(400).json({ message: 'Specified id is not valid' });
-        return;
-      }
-
-      Product.findByIdAndDelete(productId)
-      .then(() => res.json({ message: `Product with ${productId} is removed successfully.` }))
-      .catch(error => res.json(error));
-
-})
-
-
-// Route to create an offer for a product //////// WORK  ////////
-router.post("/products/:productId/offer", isAuthenticated, (req, res, next) => {
-
   const { productId } = req.params
 
-  const { price, message } = req.body
-
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-
-    let offer;
-
-    Offer.create({price, message, user: req.payload._id, product: productId})
-    .then(response =>{
-      offer = response
-      return User.findByIdAndUpdate(req.payload._id, {$push: {offer: response._id}, },{new: true})
-      })
-      .then(() => {
-        return Product.findByIdAndUpdate(productId, {$push: {offer: offer._id}, },{new: true} )
-      })
-      .then(() => {
-        res.json(response.data)
-      })
-      .catch(err => res.json(err))
-      
+  Product.findByIdAndDelete(productId)
+  .then(() => res.json({ message: `Product with ${productId} is removed successfully.` }))
+  .catch(error => res.json(error));
 })
 
-// Route to get the offer for a product //////// WORK  ////////
-router.get("/products/:productId/offer", (req, res, next) => {
-
-  const { productId } = req.params
-
-  if (!mongoose.Types.ObjectId.isValid(productId)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-
-    Offer.find()
-    .then(response => {
-      res.json(response)
-    })
-    .catch(err => res.json(err))
-
-})
 
 // Route get product details for checkout by ID //////// WORK  ////////
 router.get("/checkout/:productId", (req, res, next) => {
@@ -193,7 +135,6 @@ router.get("/checkout/:productId", (req, res, next) => {
     res.json(response)
   })
   .catch(err => res.json(err))
-
 })
 
 // Route to get products based on query
@@ -202,11 +143,11 @@ router.get("/search", (req, res, next) => {
   const { q } = req.query
 
   Product.find({ $or: [{ title: {$regex: q, $options: "i" } }, { description: {$regex: q, $options: "i" } }] })
-    .populate("user")
-    .then(allProducts => {
-      res.json(allProducts)
-    })
-    .catch(err => res.json(err))
+  .populate("user")
+  .then(allProducts => {
+    res.json(allProducts)
+  })
+  .catch(err => res.json(err))
 })
 
 // Route to check whether a user likes a product already or not
@@ -230,7 +171,6 @@ router.post("/products/:productId/like-check", (req, res, next) => {
     }
   })
   .catch(err => res.json(err))
-      
 })
 
 // Route to add a like to the user's Like document (and create it on the very first like event)
@@ -268,7 +208,6 @@ router.post("/products/:productId/like", (req, res, next) => {
     }
   })
   .catch(err => res.json(err))
-      
 })
 
 // Route to un-like a product
